34aa23a204fcef3620729649aa64bbd57e810eab
carloscontrolid <carlos.veloso@controlid.com.br>
2025-04-09 16:00:33 -0300

    terraform - enable ports for app

--- network.tf
+++ network.tf
@@ -160,3 +160,44 @@ resource "aws_security_group" "idsecure-sg-portainer" {
     CostCenter  = var.costcenter
   }
 }
+
+resource "aws_security_group" "idsecure-sg-apps" {
+  vpc_id = aws_vpc.idsecure-vpc.id
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = var.trusted_ips
+    description = "Allow APP ports from trusted IPs"
+  }
+
+  ingress {
+    from_port   = 5002
+    to_port     = 5002
+    protocol    = "tcp"
+    cidr_blocks = var.trusted_ips
+    description = "Allow APP ports from trusted IPs"
+  }
+
+  ingress {
+    from_port   = 5003
+    to_port     = 5003
+    protocol    = "tcp"
+    cidr_blocks = var.trusted_ips
+    description = "Allow APP ports from trusted IPs"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "idsecure-sg-apps"
+    Terraformed = var.terraform_tag
+    CostCenter  = var.costcenter
+  }
+}
