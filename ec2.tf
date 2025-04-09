df32f5376f6290fa8b7a5430bfffaf5d870f4a4f
carloscontrolid <carlos.veloso@controlid.com.br>
2025-04-09 16:02:08 -0300

    terraform - add ec2 to idsecure-sg-apps

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "idsecure-homolog-01" {
   ami                    = var.ubuntu_ami_id
   instance_type          = var.instance_type
   key_name               = aws_key_pair.terraform_key_pub.key_name
-  vpc_security_group_ids = [aws_security_group.idsecure-sg-ssh.id, aws_security_group.idsecure-sg-portainer.id]
+  vpc_security_group_ids = [aws_security_group.idsecure-sg-ssh.id, aws_security_group.idsecure-sg-portainer.id, aws_security_group.idsecure-sg-apps.id]
   subnet_id              = aws_subnet.idsecure-public_subnet_a.id
   availability_zone      = var.availability_zone
   associate_public_ip_address = true
