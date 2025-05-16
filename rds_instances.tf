1cd4e34f97c6f8b9a0b32dae90880feedd82a4ff
carloscontrolid <carlos.veloso@controlid.com.br>
2025-05-16 18:44:53 -0300

    terraform: add db flag apply_immediately

--- rds_instances.tf
+++ rds_instances.tf
@@ -14,6 +14,7 @@ resource "aws_db_instance" "idsecure-mysql-rds" {
   publicly_accessible    = true
   skip_final_snapshot    = true
   port                   = 3306
+  apply_immediately      = true
 
   tags = {
     Name        = "idsecure-mysql-db"
