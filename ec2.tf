021357cb357230679bfdcdf9b4c351b2089cd7e7
carloscontrolid <carlos.veloso@controlid.com.br>
2025-06-03 18:14:49 -0300

    terraform: remove windows machine

--- ec2.tf
+++ ec2.tf
@@ -17,19 +17,19 @@ resource "aws_instance" "idsecure-homolog-01" {
   }
 }
 
-resource "aws_instance" "idsecure-windows-01" {
-  ami                    = var.windows_ami_id
-  instance_type          = var.windows_instance_type
-  key_name               = aws_key_pair.terraform_key_pub.key_name
-  vpc_security_group_ids = [aws_security_group.idsecure-sg-rdp.id]
-  subnet_id              = aws_subnet.idsecure-public_subnet_a.id
-  availability_zone      = var.availability_zone
-  associate_public_ip_address = true
+# resource "aws_instance" "idsecure-windows-01" {
+#   ami                    = var.windows_ami_id
+#   instance_type          = var.windows_instance_type
+#   key_name               = aws_key_pair.terraform_key_pub.key_name
+#   vpc_security_group_ids = [aws_security_group.idsecure-sg-rdp.id]
+#   subnet_id              = aws_subnet.idsecure-public_subnet_a.id
+#   availability_zone      = var.availability_zone
+#   associate_public_ip_address = true
 
-  tags = {
-    Name        = "idsecure-windows-01"
-    RDP         = true
-    Terraformed = var.terraform_tag
-    CostCenter  = var.costcenter
-  }
-}
+#   tags = {
+#     Name        = "idsecure-windows-01"
+#     RDP         = true
+#     Terraformed = var.terraform_tag
+#     CostCenter  = var.costcenter
+#   }
+# }
