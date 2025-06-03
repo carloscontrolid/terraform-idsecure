15d4f4f64edff07952e3b785b85421885659658f
carloscontrolid <carlos.veloso@controlid.com.br>
2025-06-03 20:54:35 -0300

    terraform: fix variables

--- pubkey.tf
+++ pubkey.tf
@@ -4,3 +4,8 @@ resource "aws_key_pair" "terraform_key_pub" {
   key_name   = "pub_key_carlos"
   public_key = var.pub_key_carlos
 }
+
+resource "aws_key_pair" "windows_server_key" {
+  key_name   = "windows_server_key"
+  public_key = var.windows_server_key
+}
